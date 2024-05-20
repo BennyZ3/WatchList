77e30f65c843be5c5c015a7f3a8b82ab955b1e57
Benny Zheng <benny@davidenergy.com>
2024-05-19 22:34:33 -0400

    added a temporary path to test error status

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Controllers
-
 // Health check route
 app.get("/", (request, response) => {
   response.status(200).json({ data: "Service is running" });
+});
+
+// TODO: remove after frontend error testing
+app.get("/error", (request, response) => {
+  console.log("Error route");
+  response.status(400).json({ error: "Error route" });
 });
 
 module.exports = app;
